fc4bfc5a74e113d8441398d2fc941f4d5bc68131
Jumer Acosta <jumeracosta@AS100704.local>
2016-08-10 21:33:17 +0800

    added the File class in script

--- lists_down_files_dir.rb
+++ lists_down_files_dir.rb
@@ -1,30 +1,20 @@
-#Dir.entries(".") do |file|
-#	puts file
-#end
-
-#puts Dir.entries( '.' ).select{ |f| File.file? f }
-
-#Dir.foreach("testdir") {|x| puts "Got #{x}" }
-
-#Dir.entries("file_name") {|x| puts "Got #{x}"}
-
-#puts Dir.glob("*/*")
-
-#files.each do |file_name|
-	
-#Dir.new('.').each {|file| puts file }
-
-#Dir.foreach('.') {|x| puts "Got #{x}" }
-
 #puts Dir.entries('.').select {|f| File.file? f} #to get the file of the current directories
 
 #puts Dir.glob("**/") #to get the path of directories upto subdirectories
 
-files = Dir[ '**/*' ].select{ |f| File.file? f } #returns filename and paths
-puts files # ["file1.rb", "file2.rb", "file3.rb"]
+#files = Dir[ '**/*' ].select{ |f| File.file? f } #returns filename and paths
+#puts files # ["file1.rb", "file2.rb", "file3.rb"]
 
-require 'csv'
+#Using the CSV class
+#require 'csv'
 
-CSV.open('test.csv', 'wb') do |csv|
-	csv << (files)
-end
+#CSV.open('test.csv', 'wb') do |csv|
+#	csv << (files)
+#end
+
+#Using the File Class
+files = Dir[ '**/*' ].select{ |f| File.file? f }
+puts files #["file1.rb", "path/file2.rb"]
+
+file = File.new('test2.csv','w')
+file.puts (files)
